b724d8bb7ba74e316209314f2f951e5f83a98a6a
Matheus Hyago <142069671+MatheusHyago@users.noreply.github.com>
2024-10-10 12:19:53 -0300

    Update Listas_4.py

--- LISTA/Listas_4.py
+++ LISTA/Listas_4.py
@@ -1,6 +1,11 @@
 lista_1 = [1,2,3,4,5]
 
-print(lista_1)
+def lista_inversa(lista_1):
+        lista_1.reverse()
+        return lista_1
 
-lista_1.reverse()
-print(lista_1)
+print (lista_1)
+
+print("+="*20)
+
+print(lista_inversa(lista_1))

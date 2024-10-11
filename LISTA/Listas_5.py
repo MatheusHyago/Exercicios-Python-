537d0b88d19d57caccba4cec2052a83373605354
Matheus Hyago <142069671+MatheusHyago@users.noreply.github.com>
2024-10-11 09:31:06 -0300

    Add files via upload

---
+++ LISTA/Listas_5.py
@@ -0,0 +1,11 @@
+lista_1 = [[0,1,2],[3,4,5],[6,7,8]]
+
+def soma_matrizes(lista_1):
+        for linha in lista_1:
+                soma= sum(linha)
+                print(f"Soma da linha {linha}: {soma}")
+
+soma_matrizes(lista_1)
+
+
+

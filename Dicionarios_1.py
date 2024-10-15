805911a4b6f638f77ae5e7ea4bd823758f897ba1
Matheus Hyago <142069671+MatheusHyago@users.noreply.github.com>
2024-10-15 10:46:44 -0300

    Add files via upload

---
+++ Dicionarios_1.py
@@ -0,0 +1,8 @@
+dicionario_1 = {
+    "Nome":"Ana",    
+    "Idade":25,
+    "cidade": "São Paulo"
+    }
+
+nome = dicionario_1 ['Nome']
+print(nome)
---
+++ Dicionarios_2.py
@@ -0,0 +1,8 @@
+dicionario_1 = {
+    "Nome":"Ana",    
+    "Idade":25,
+    "cidade": "São Paulo"
+    }
+
+idade = dicionario_1 ['Idade']
+print(idade)

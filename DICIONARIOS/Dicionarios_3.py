33c37227878bcd8dcd6a248058b6b0910c6d2b19
Matheus Hyago <142069671+MatheusHyago@users.noreply.github.com>
2024-10-23 14:54:43 +0000

    exercicios 3 / 4

---
+++ DICIONARIOS/Dicionarios_3.py
@@ -0,0 +1,13 @@
+dicionario_1 = {
+    "Nome":"Ana",    
+    "Idade":25,
+    "cidade": "São Paulo"
+    }
+
+idade = dicionario_1 ['Idade']
+print(idade)
+
+dicionario_1 ['profissão'] = 'engenheira'
+
+print(dicionario_1)
+
---
+++ DICIONARIOS/Dicionarios_4.py
@@ -0,0 +1,13 @@
+dicionario_1 = {
+    "Nome":"Ana",    
+    "Idade":25,
+    "cidade": "São Paulo"
+    }
+
+idade = dicionario_1 ['Idade']
+print(idade)
+
+del dicionario_1 ['cidade']
+
+print(dicionario_1)
+
